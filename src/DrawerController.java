8ad00d2b16497ac9c3991ed155e6bdb5c554c4ff
Oskar Pol <oskarbank@gmail.com>
2017-03-22 19:13:55 +0100

    Drawer

---
+++ src/DrawerController.java
@@ -0,0 +1,31 @@
+import com.jfoenix.controls.JFXButton;
+import javafx.fxml.FXML;
+
+/**
+ * Created by OskarPraca on 2017-03-22.
+ */
+public class DrawerController {
+
+    @FXML
+    public JFXButton button1;
+    @FXML
+    public JFXButton button2;
+    @FXML
+    public JFXButton button3;
+    @FXML
+    public JFXButton button4;
+
+    public void clickButton1(){
+
+    }
+    public void clickButton2(){
+
+    }
+    public void clickButton3(){
+
+    }
+    public void clickButton4(){
+
+    }
+
+}
--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,40 +1,78 @@
 package sample;
 
+import com.jfoenix.controls.JFXDrawer;
 import javafx.animation.FadeTransition;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
 import javafx.scene.control.Alert;
+import javafx.scene.control.ButtonBar;
 import javafx.scene.control.ButtonType;
+import javafx.scene.control.TextInputDialog;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.VBox;
 import javafx.util.Duration;
 
+import java.io.IOException;
+import java.net.URL;
 import java.util.Optional;
+import java.util.ResourceBundle;
 
 
-public class Controller {
+public class Controller implements Initializable{
 
     @FXML
     public ImageView imageLogo;
 
+    @FXML
+    public JFXDrawer fxDrawer;
+
+
+
     public void buttonStartClicked(MouseEvent event) {
-       if(event.isControlDown()){
-           openWindow();
-      }
+      // if(event.isControlDown()){
+           if(fxDrawer.isShown()) {
+              fxDrawer.close();
+           }else{
+               fxDrawer.open();
+           }
+     // }
     }
 
     private  void openWindow() {
-        Alert alert = new Alert(Alert.AlertType.INFORMATION);
-        alert.setTitle("AkedmiaKodu ostrzega..");
-        alert.setHeaderText("Ostrzegamy..");
-        alert.setContentText("Dziś będzie padać.");
+        Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
+        alert.setTitle("AkedmiaKodu");
+        alert.setHeaderText("Wybierz jedną z opcji");
+
+        ButtonType buttonOne = new ButtonType("Pierwszy przycisk");
+        ButtonType buttonTwo = new ButtonType("Drugi przycisk");
+        ButtonType buttonThree = new ButtonType("Trzeci przycisk");
+        ButtonType buttonCancel = new ButtonType("Anuluj", ButtonBar.ButtonData.CANCEL_CLOSE);
+
+        alert.getButtonTypes().setAll(buttonOne, buttonTwo, buttonThree, buttonCancel);
+
 
         Optional<ButtonType> optional = alert.showAndWait();
-        if(optional.get() == ButtonType.OK) {
-          System.out.println("Kliknięto ok");
+        if(optional.get() == buttonCancel) {
+            alert.close();
+            System.out.println("Ktos zamknal okno bez wyboru");
         }
     }
+
+    public void openWindowWithText() {
+        TextInputDialog dialog = new TextInputDialog("Wpisz imie");
+        dialog.setTitle("Podaj nam swoje imie");
+        dialog.setContentText("Wpisz imię: ");
+
+        Optional<String> result = dialog.showAndWait();
+        result.ifPresent(name -> System.out.println("Twoje imie to" + name));
+
+    }
+
+
 
     private void runAnimation() {
         FadeTransition fadeInTransition = new FadeTransition(Duration.seconds(5), imageLogo);
@@ -53,5 +91,18 @@ public class Controller {
         });
         fadeInTransition.play();
 
+
+
+
+    }
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        try {
+            VBox box = FXMLLoader.load(getClass().getResource("drawerContent.fxml"));
+            fxDrawer.setSidePane(box);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
